5f463bfbc54bce2070112734aa470399a3444fdf
Chad Dibble <chad@chadthebad.com>
2012-06-01 14:15:41 -0700

    Modified Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -51,10 +51,10 @@ Checkins::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  # 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'static#index'
 
   # See how all your routes lay out with "rake routes"
 
