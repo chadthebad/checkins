202c9eb80a392b9c55482bec8818b98af4d0859a
Chad Dibble <chad@chadthebad.com>
2012-06-01 13:46:49 -0700

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkins::Application.config.secret_token = 'cd243792e30cecc0e39e4e162242feb404377d07f710373a5694afbb56a9c22d612a73f68a3ef761eef69110ced99b15efb0f9620f3f52d7aef2a7afe207dc94'
